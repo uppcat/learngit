f39a0dc8eb7d2f83ba0405cc6cb4420701d70541
uppcat <bigmaxgod@qq.com>
2019-05-29 02:07:10 +0800

    第一次修改

---
+++ test.py
@@ -0,0 +1,10 @@
+# Author:jiang
+# -*- coding: utf-8 -*-
+# @Time : 2019-05-29 1:56
+# @File : test.py
+# @Software: PyCharm
+
+def func():
+    print("this is test for git")
+if __name__=="__main__":
+    func()
